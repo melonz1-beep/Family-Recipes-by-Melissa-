c69e071db57fca8cee7de0bb9a91a724cab541a7
melonz1-beep <mlehr1211@gmail.com>
2026-07-30 17:53:25 -0400

    Refresh recipe binder cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE="melissa-recipe-binder-lifelike-icons-v3";
+const CACHE="melissa-recipe-binder-lifelike-icons-v4";
 const FILES=["./","index.html","style.css","app.js","manifest.json","sw.js","icon-192.png","icon-512.png","splash-logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
