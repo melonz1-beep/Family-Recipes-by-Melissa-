1d6579f1d41c7caa594c62341072e39d3f280c85
melonz1-beep <mlehr1211@gmail.com>
2026-08-04 07:20:03 -0400

    Refresh recipe binder cache for kielbasa recipes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="melissa-recipe-binder-lifelike-icons-v4";
+const CACHE="melissa-recipe-binder-lifelike-icons-v5";
 const FILES=["./","index.html","style.css","app.js","manifest.json","sw.js","icon-192.png","icon-512.png","splash-logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
