eff96879a68e251df7b82f0fb84c756327e59a05
melonz1-beep <mlehr1211@gmail.com>
2026-08-04 07:35:23 -0400

    Refresh recipe binder cache for meatballs recipe

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="melissa-recipe-binder-lifelike-icons-v7";
-const FILES=["./","index.html","style.css","app.js","manifest.json","sw.js","icon-192.png","icon-512.png","splash-logo.png"];
+const CACHE="melissa-recipe-binder-lifelike-icons-v8";
+const FILES=["./","index.html","style.css","app.js","recipe-spicy-slow-cooker-meatballs.js","manifest.json","sw.js","icon-192.png","icon-512.png","splash-logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
